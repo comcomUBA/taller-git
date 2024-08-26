152a3c9581eebbe6db48db3c0df52d8f2df9053b
jugoprex <juligoria@gmail.com>
2024-08-25 22:28:33 -0300

    script ejercicio 1

---
+++ clases/script_ejemplo.py
@@ -0,0 +1,8 @@
+# Bienvenido al taller de Git!
+# Este es un script de ejemplo
+
+# reemplazá el texto por otro que quieras imprimir en pantalla
+# no olvides no borrar las comillas 
+
+print('Hello world!')
+
